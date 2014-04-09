84bcf096b3fd9dbbf97863fa24ebf0aca1618853
David Barbour <dmbarbour@gmail.com>
2014-04-09 15:20:13 -0500

    added a Read instance (based on parser combinators)

--- hsrc/ABC/Operators.hs
+++ hsrc/ABC/Operators.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE PatternGuards  #-}
 
 -- | pure description of ABC operators
+--   plus useful Show and Read instances
 module ABC.Operators
     ( OpC(..), Op(..)
-    , opCharList, showOps
-    , abcQuoteNum, abcEscapeText
+    , opCharList
+    , abcQuoteNum
     ) where
 
+--import Text.Show
+-- import qualified Text.Read
+import Control.Applicative ((<$>))
+import Text.Read (Read(..))
+import qualified Text.ParserCombinators.ReadP as R
+import qualified Text.ParserCombinators.ReadPrec as RP
 import Data.Ratio
 import qualified Data.List as L
 
@@ -25,6 +32,16 @@ data OpC
     | Op_SP | Op_LF -- ' ' and '\n' (identity)
     deriving (Eq, Ord)
 
+-- Op: pure ABC operations
+data Op
+    = OpC !OpC    -- single character op
+    | BL  [Op]    -- block literals, e.g.  `[r[^'wol]wo^'wol]`
+    | TL  String  -- text literal (with escapes removed)
+    | Tok String  -- {foo}; invoke capability with given token
+    deriving (Eq, Ord)
+-- Note: ABC token strings may not contain '{', '}', or LF ('\n')
+--   tokens are typically prefixed to indicate interpretation
+
 opCharList :: [(OpC,Char)]
 opCharList =
     [(Op_l,'l'),(Op_r,'r'),(Op_w,'w'),(Op_z,'z'),(Op_v,'v'),(Op_c,'c')
@@ -38,16 +55,6 @@ opCharList =
     ,(Op_d5,'5'),(Op_d6,'6'),(Op_d7,'7'),(Op_d8,'8'),(Op_d9,'9')
     ,(Op_SP,' '),(Op_LF,'\n')
     ]
-
--- Op: pure ABC operations
-data Op
-    = OpC !OpC   -- single character op
-    | BL  [Op]    -- block literals, e.g.  `[r[^'wol]wo^'wol]`
-    | TL  String  -- text literal (with escapes removed)
-    | Tok String -- {foo}; invoke capability with given token
-    deriving (Eq, Ord)
--- Note: ABC token strings may not contain '{', '}', or LF ('\n')
---   tokens are typically prefixed to indicate interpretation
 
 -- | translate a number into ABC's pseudo-literal syntax
 abcQuoteNum :: Rational -> [Op]
@@ -78,36 +85,81 @@ opd 8 = Op_d8
 opd 9 = Op_d9
 opd _ = error "invalid digit"
 
-instance Show OpC where show = (:[]) . showOpC
-instance Show Op where show = showOp
-
-showOps :: [Op] -> String
-showOps = L.concatMap showOp
-
-showOp :: Op -> String
-showOp (OpC opc) = [showOpC opc]
-showOp (BL ops) = "[" ++ showOps ops ++ "]"
-showOp (TL txt) = abcEscapeText txt
-showOp (Tok txt) = "{" ++ txt ++ "}" 
-
-showOpC :: OpC -> Char
-showOpC op = 
-    case L.lookup op opCharList of
+instance Show OpC where 
+    showsPrec _ opc = case L.lookup opc opCharList of
         Nothing -> error "opCharList is missing an operator"
-        Just c -> c
+        Just c -> (c:)
+    showList = showConcatList
+instance Show Op where
+    showsPrec _ (OpC opc) = shows opc
+    showsPrec _ (BL ops) =
+        showChar '[' .
+        showList ops .
+        showChar ']'
+    showsPrec _ (TL txt) =
+        showChar '"' .
+        showEscaped txt .
+        showChar '\n' . showChar '~'
+    showsPrec _ (Tok txt) = showChar '{' . showString txt . showChar '}'
+    showList = showConcatList
 
--- | Escape text for use as an ABC literal
-abcEscapeText :: String -> String
-abcEscapeText = wrap . L.intercalate "\n " . lines1 where
-    wrap s = "\"" ++ s ++ "\n~"
+-- show a list without the square brackets or commas
+-- requires 'showsPrec' to be defined, for best performance
+showConcatList :: (Show a) => [a] -> ShowS
+showConcatList (x:xs) = shows x . showConcatList xs
+showConcatList [] = id
 
--- lines1 guarantees at least one line and returns a blank line if
--- (unlike Data.List.lines, which returns no lines for empty string)
-lines1 :: String -> [String]
-lines1 = l1 where
-    l1 s = 
-        let (hd,more) = L.span (/= '\n') s in
-        hd : l2 more
-    l2 ('\n':s) = l1 s
-    l2 [] = []
-    l2 _ = error "illegal state in lines1"
+-- escape text for showing a text literal
+-- in ABC, only the '\n' characters need be escaped
+-- does not include wrappers
+showEscaped :: String -> ShowS
+showEscaped ('\n':ss) = showChar '\n' . showChar ' ' . showEscaped ss
+showEscaped (c:ss) = showChar c . showEscaped ss
+showEscaped [] = id
+
+instance Read OpC where
+    readPrec = RP.lift readOpC
+    readListPrec = RP.lift (R.many readOpC)
+instance Read Op where
+    readPrec = RP.lift readOp
+    readListPrec = RP.lift (R.many readOp)
+
+swap2 :: (a,b) -> (b,a)
+swap2 (a,b) = (b,a)
+
+readOpC :: R.ReadP OpC
+readOpC =
+    R.get >>= \ c ->
+    case L.lookup c (fmap swap2 opCharList) of
+        Nothing -> R.pfail
+        Just opc -> return opc
+
+readOp :: R.ReadP Op
+readOp = 
+    (BL <$> readBlock) R.<++
+    (TL <$> readText) R.<++
+    (Tok <$> readTok) R.<++
+    (OpC <$> readOpC) 
+
+readBlock :: R.ReadP [Op]
+readBlock =
+    R.char '[' >>
+    R.many readOp >>= \ ops ->
+    R.char ']' >>
+    return ops
+
+-- readText will remove the escapes (only LF is escaped)
+readText :: R.ReadP String
+readText = start where
+    start = 
+        R.char '"' >>
+        textLine >>= \ t0 ->
+        R.manyTill (R.char ' ' >> textLine) (R.char '~') >>= \ ts ->
+        return $ L.concat (t0 : map ('\n':) ts)
+    textLine = R.manyTill R.get (R.char '\n')
+
+readTok :: R.ReadP String
+readTok = R.char '{' >> R.manyTill (R.satisfy isTokChr) (R.char '}') 
+
+isTokChr :: Char -> Bool
+isTokChr c = not $ ('{' == c || '\n' == c || '}' == c)
