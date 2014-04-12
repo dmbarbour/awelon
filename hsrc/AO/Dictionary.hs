a576d05b1116d95d5e7eeec078b8f10eea61e2b7
David Barbour <dmbarbour@gmail.com>
2014-04-12 13:09:21 -0500

    preliminary dictionary file (still considering fresh design here)

---
+++ hsrc/AO/Dictionary.hs
@@ -0,0 +1,10 @@
+
+-- | A minimal AO dictionary is `Map Word AO_Code`. However, it can
+-- be difficult to keep such a dictionary 'clean' with respect to
+-- cycles and keeping extra source information.
+--
+module AO.Dictionary
+    (
+    ) where
+
+
