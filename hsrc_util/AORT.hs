11a31c875ba37fbc13deec73a16db098dc47ba63
David Barbour <dmbarbour@gmail.com>
2014-04-22 11:26:14 -0500

    added most of aoi's old effects (readFile, writeFile, etc.)

--- hsrc_util/AORT.hs
+++ hsrc_util/AORT.hs
@@ -2,32 +2,29 @@
 
 -- | A runtime monad for the 'ao' and 'aoi' executables. At the top
 -- level, 'ao' and 'aoi' handle streaming ABC operations without any
--- direct effects. 
+-- direct effects.
 --
--- Long term behavior will tentatively be modeled by installation of
--- RDP behaviors to manage resources. This is a step towards Awelon 
--- project goals.
+-- Long-term behavior will tentatively be modeled by installation of
+-- RDP behaviors to manage resources, as per Awelon project.
 --
--- Useful goals for the design at this layer:
+-- Desiderata at this layer:
 --
 -- * support for 'undo' in the aoi REPL
--- * support for persistent sessions  
+-- * support for persistent sessions (maybe)
 --
--- These goals require stable capabilities from step to step. Also,
--- it requires that state associated with the capabilities be either 
--- stored in the capability itself, inferrable from the environment,
--- or managed explicitly.
---
--- The other primary goal is performance, such that performance is
--- no longer an obstacle to application development in AO language.
--- AORT will support JIT, and might eventually support some GPGPU
--- computation.
+-- The primary goal is performance. Performance has been an obstacle
+-- to development of applications in AO. If JIT and other features can
+-- eliminate this obstacle, AO will be better situated for success.
 -- 
 module AORT
     ( AORT, AORT_CX, readRT, liftRT, runRT, liftIO
+
+    -- eventually, I might support some configuration
+    --  but isn't needed while 'ao' and 'aoi' are only
+    --  clients and one goal is equivalent behavior
     , newDefaultRuntime
     , newDefaultEnvironment
-    , aoStdEnv
+    , newPowerBlock, aoStdEnv
     ) where
 
 import Control.Applicative
@@ -39,13 +36,24 @@ import Data.IORef (IORef)
 import qualified Data.IORef as IORef
 import Control.Concurrent.MVar (MVar)
 import qualified Control.Concurrent.MVar as MVar
-import System.IO.Unsafe (unsafeInterleaveIO)
-import qualified System.IO as Sys
 
+-- import System.IO.Unsafe (unsafeInterleaveIO)
+import qualified System.IO as Sys
+import qualified System.Entropy as Entropy
+import qualified System.Environment as Env
+import qualified System.IO.Error as Err
+import qualified Filesystem.Path.CurrentOS as FS
+import qualified Filesystem as FS
+
+import Data.Ratio
 import qualified Data.Sequence as S
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Map as M
+import qualified Data.ByteString.Base64.URL as B64
 
 import ABC.Operators
 import ABC.Imperative.Value
@@ -67,11 +75,10 @@ newtype AORT a = AORT (ReaderT AORT_CX IO a)
 -- At the moment, this is a place holder. But I expect I'll eventually
 -- need considerable context to manage concurrency and resources.
 data AORT_CX = AORT_CX
-    { aort_lcaps  :: MVar (M.Map Token (Prog AORT)) 
-    , aort_anno   :: String -> Maybe (Prog AORT)
-    , aort_gensym :: IO Token
+    { aort_anno   :: String -> Maybe (Prog AORT)
+    , aort_power  :: String -> Maybe (Prog AORT)
+    , aort_secret :: String -- a random string
     } 
-type Token = String
 
 -- | run an arbitrary AORT program.
 runRT :: AORT_CX -> AORT a -> IO a
@@ -85,100 +92,20 @@ readRT = AORT . asks
 liftRT :: (AORT_CX -> IO a) -> AORT a
 liftRT = AORT . ReaderT
 
+bytesToB64 :: ByteString -> String
+bytesToB64 = B.unpack . B64.encode
+
 -- | a new runtime with default settings
 newDefaultRuntime :: IO AORT_CX
 newDefaultRuntime = 
-    MVar.newMVar M.empty >>= \ mvCaps ->
-    newDefaultGenSym >>= \ gensym ->
-    let cx = AORT_CX { aort_lcaps  = mvCaps
-                     , aort_anno   = defaultAnno
-                     , aort_gensym = gensym
+    bytesToB64 <$> Entropy.getEntropy 12 >>= \ secret ->
+    let cx = AORT_CX { aort_anno   = defaultAnno
+                     , aort_power  = defaultPower
+                     , aort_secret = secret
                      }
     in return cx
 
--- | Create a linear capability that will execute a particular program
--- when applied. Effects can be modeled through use of the capability.
--- This capability should be safe for serialization, e.g. JIT. At the
--- moment, however, it is not secure (because AO gensym is not secure).
-newLinearCap :: String -> Prog AORT -> AORT (Block AORT)
-newLinearCap debugHint prog =
-    liftRT aort_gensym >>= \ tok0 -> 
-    let showHint = (showString . clnTok) debugHint in
-    let token = (showChar '!' . showHint . showChar ' ') tok0 in
-    let b = Block { b_aff = True, b_rel = True
-                  , b_code = S.singleton (Tok token)
-                  , b_prog = invoke token }
-    in
-    installLinearCap token prog >>
-    return b
-
--- add a linear capability to the search map
-installLinearCap :: String -> Prog AORT -> AORT ()
-installLinearCap tok prog = liftRT (mod ins . aort_lcaps) where
-    mod fn mv = MVar.takeMVar mv >>= MVar.putMVar mv . fn
-    ins = M.insert tok prog
-
-extractLinearCap :: String -> AORT (Prog AORT)
-extractLinearCap tok =
-    readRT aort_lcaps >>= \ mv ->
-    liftIO (MVar.takeMVar mv) >>= \ lcs ->
-    case M.lookup tok lcs of
-        Nothing -> 
-            liftIO (MVar.putMVar mv lcs) >> 
-            fail ("unrecognized token: {" ++ show tok ++ "}")
-        Just prog ->
-            liftIO (MVar.putMVar mv (M.delete tok lcs)) >>
-            return prog
-
--- forbid use of '{', '\n', and '}' in token debug hints
-clnTok :: String -> String
-clnTok = fmap mc where
-    mc '{' = '('
-    mc '}' = ')'
-    mc '\n' = ';'
-    mc c = c
-
--- | create a new unique-symbol generator
--- (todo: create SECURE symbol generator)
-newDefaultGenSym :: IO (IO Token)
-newDefaultGenSym = incsym <$> MVar.newMVar 0 where
-    incsym :: MVar Integer -> IO Token
-    incsym mv = 
-        MVar.takeMVar mv >>= \ n ->
-        let tok = show n in
-        let n' = (n+1) in
-        n' `seq` MVar.putMVar mv n' >>
-        return tok
-
--- | an AO 'environment' is simply the first value passed to the
--- program. The AO standard environment has the form:
---
---    (stack * (hand * (power * ((stackName * namedStacks) * ext)
---
--- which provides some useful scratch spaces for a running program. 
---
--- The normal use case is that this environment is initially empty
--- except for a powerblock, and inputs are primarily supplied by 
--- side-effects. However, a few initial arguments might be placed on
--- the stack in some non-standard use cases.
---
-aoStdEnv :: Block cx -> V cx
-aoStdEnv pb = (P U (P U (P (B pb) (P (P sn U) U))))
-    where sn = textToVal "" -- L U
-
--- | create a standard environment with a default powerblock
-newDefaultEnvironment :: AORT (V AORT)
-newDefaultEnvironment = aoStdEnv <$> newDefaultPB
-
--- | obtain a block representing access to default AORT powers.
---
--- default powers are in development; cf doc ProcessModel.md
---
-newDefaultPB :: AORT (Block AORT) 
-newDefaultPB = newLinearCap "AORT" ((=<<) run) where
-    run v = fail $ "AORT has no effects yet: " ++ show v
-
--- | default annotations support for AORT
+-- default annotations support for AORT
 --
 -- To add: 
 --
@@ -200,15 +127,110 @@ debugPrintRaw v =  liftIO $ Sys.hPutStrLn Sys.stderr (show v)
 debugPrintText (valToText -> Just txt) = liftIO $ Sys.hPutStr Sys.stderr txt
 debugPrintText v = fail $ "{&debug print text} @ " ++ show v
 
+-- | an AO 'environment' is simply the first value passed to the
+-- program. The AO standard environment has the form:
+--
+--    (stack * (hand * (power * ((stackName * namedStacks) * ext)
+--
+-- which provides some useful scratch spaces for a running program. 
+--
+-- The normal use case is that this environment is initially empty
+-- except for a powerblock, and inputs are primarily supplied by 
+-- side-effects. However, a few initial arguments might be placed on
+-- the stack in some non-standard use cases.
+--
+aoStdEnv :: Block cx -> V cx
+aoStdEnv pb = (P U (P U (P (B pb) (P (P sn U) U))))
+    where sn = textToVal "" -- L U
+
+-- | create a standard environment with a default powerblock
+newDefaultEnvironment :: AORT (V AORT)
+newDefaultEnvironment = aoStdEnv <$> newPowerBlock
+
+-- | obtain a block representing access to default AORT powers.
+newPowerBlock :: AORT (Block AORT) 
+newPowerBlock = 
+    readRT aort_secret >>= \ s ->
+    let pbTok = '!':s in
+    return $  Block { b_code = S.singleton (Tok pbTok)
+                    , b_prog = invoke pbTok -- keeping it simple
+                    , b_aff = True, b_rel = True }
+
+
+-- Default powers use the command string, rather than the token.
+-- Toplevel powers are mostly used for one-off reads and writes.
+defaultPower :: String -> Maybe (Prog AORT)
+defaultPower = flip M.lookup $ M.fromList $
+    [("randomBytes", (=<<) getRandomBytes)
+    ,("destroy", const (return U))
+    ,("getOSEnv", (=<<) getOSEnv)
+    ,("readFile", (=<<) aoReadFile)
+    ,("writeFile", (=<<) aoWriteFile)
+    ]
+
+getRandomBytes :: (MonadIO m, Applicative m) => V m -> m (V m)
+getOSEnv, aoReadFile, aoWriteFile :: (MonadIO m, Applicative m) => V m -> m (V m)
+
+getRandomBytes (N r) | ((r >= 0) && (1 == denominator r)) =
+    let nBytes = fromInteger $ numerator r in
+    let getBytes = Entropy.getEntropy nBytes in
+    textToVal . B.unpack <$> liftIO getBytes
+getRandomBytes v = fail $ "randomBytes @ " ++ show v
+
+getOSEnv (valToText -> Just var) = textToVal <$> liftIO gv where
+    gv = maybe "" id <$> tryIO (Env.getEnv var)
+getOSEnv v = fail $ "getOSEnv @ " ++ show v
+
+aoReadFile (valToText -> Just fname) =
+    let fp = FS.fromText (T.pack fname) in
+    let rf = T.unpack <$> FS.readTextFile fp in
+    maybe (L U) (R . textToVal) <$> liftIO (tryIO rf)
+aoReadFile v = fail $ "readFile @ " ++ show v
+
+aoWriteFile (P (valToText -> Just fname) (valToText -> Just content)) =
+    let fp = FS.fromText (T.pack fname) in
+    let wOp = FS.createTree (FS.directory fp) >>
+              FS.writeTextFile fp (T.pack content) 
+    in
+    let asBoolean = maybe (L U) (const (R U)) in
+    asBoolean <$> liftIO (tryIO wOp)
+aoWriteFile v = fail $ "writeFile @ " ++ show v
+    
+tryIO :: IO a -> IO (Maybe a)
+tryIO op = Err.catchIOError (Just <$> op) (const (return Nothing))
+
+execPower :: V AORT -> AORT (V AORT)
+execPower (P (valToText -> Just cmd) arg) = 
+    execCmd cmd arg >>= \ result ->
+    newPowerBlock >>= \ pb ->
+    return (P (B pb) result)
+execPower v = fail $ "power not recognized: " ++ show v
+
+execAnno :: String -> Prog AORT
+execAnno s arg = 
+    readRT aort_anno >>= \ annoFn ->
+    case annoFn s of
+        Just fn -> fn arg
+        Nothing -> id arg
+
+-- command and arg, returning result
+execCmd :: String -> V AORT -> AORT (V AORT)
+execCmd ('&':s) arg = execAnno s (pure arg) -- every annotation is a command
+execCmd cmd arg = 
+    readRT aort_power >>= \ hasCmd ->
+    case hasCmd cmd of
+        Just op -> op (pure arg)
+        Nothing -> fail $ "command not recognized: " ++ cmd
+
+execPowerTok :: String -> Prog AORT
+execPowerTok s arg =
+    readRT aort_secret >>= \ secret ->
+    if (s == secret) then arg >>= execPower else
+    fail $ "token not recognized: {!" ++ s ++ "}"
+
 -- ... enough to get started for now ...
 instance Runtime AORT where
-    invoke ('&':s) = \ arg ->
-        readRT aort_anno >>= \ annoFn ->
-        case annoFn s of
-            Just fn -> fn arg
-            Nothing -> id arg
-    invoke lc@('!':_) = \ arg ->
-        extractLinearCap lc >>= \ prog ->
-        prog arg
+    invoke ('&':s) = execAnno s
+    invoke ('!':s) = execPowerTok s
     invoke s = invokeFails s
 
