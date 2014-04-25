a73e7ae4a58f7b32217d1a47590fec46bc7c964e
David Barbour <dmbarbour@gmail.com>
2014-04-25 16:21:23 -0500

    beginning development of JIT (finally!)

---
+++ hsrc/ABC/Imperative/JIT.hs
@@ -0,0 +1,9 @@
+
+
+module ABC.Imperative.JIT 
+    ( abc2hs_naive
+    , jit_naive
+    ) where
+
+abc2hs_naive = error "todo: ABC to Haskell (naive)"
+jit_naive = error "todo: JIT an ABC program (naive)"
