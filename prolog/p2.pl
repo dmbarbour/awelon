f5e3f8e24f0b56ac18eeae3251fb59b877b230c7
David Barbour <dmbarbour@gmail.com>
2014-07-30 10:44:44 -0500

    dip2

--- prolog/p2.pl
+++ prolog/p2.pl
@@ -20,6 +20,8 @@ prim([A,[B,[C,D]]], z, [A,[C,[B,D]]]).
 prim(A,v,[A,unit]).
 prim([A,unit],c,A).
 
+dip2_pre(N) :- path( [ [fn,[a,[b,s]]],e ], [fn, [[s,e], [[b,a],unit]]], N).
+dip2_post(N) :- path( [[s,e], [[b,a],unit]], [[a,[b,s]],e], N).
 %lib(A,id,A,[]).
 
 %lib([A,[B,[C,D]]], wzw, [C,[B,[A,D]]], [w,z,w]). % rotx
